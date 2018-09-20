471b80df3801a8b1dc114a2176c78ac8156fabb6
Jether Rodrigues <jether.rodrigues_fipcerrado@inpe.br>
2018-09-20 17:00:50 -0300

    atualizacao

--- js/terrabrasilis-start.js
+++ js/terrabrasilis-start.js
@@ -23,3 +23,23 @@ $(document).ready(function () {
         graph.updateChartsDimensions();
     });
 });
+
+/* Altera icone do accordion da tabela */
+
+$(document).ready(function () {
+            $('.collapse')
+                .on('shown.bs.collapse', function() {
+                    $(this)
+                        .parent()
+                        .find(".fa-plus-circle")
+                        .removeClass("fa-plus-circle")
+                        .addClass("fa-minus-circle");
+                })
+                .on('hidden.bs.collapse', function() {
+                    $(this)
+                        .parent()
+                        .find(".fa-minus-circle")
+                        .removeClass("fa-minus-circle")
+                        .addClass("fa-plus-circle");
+                });
+        });
