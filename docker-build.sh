502057cd59e12b8b663f8d60324a2641080faf67
André Carvalho <afacarvalho@yahoo.com.br>
2018-09-24 19:59:22 -0300

    Chnages in docker environment

---
+++ docker-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Stopping all containers
+docker container stop terrabrasilis_dashboard_increments
+docker container stop terrabrasilis_dashboard_aggregated
+docker container stop terrabrasilis_dashboard_daily
+
+# build all images
+docker build -t terrabrasilis/dashboard-increments:alpha --build-arg INDEX_FILE=prodes-cerrado-rates -f environment/Dockerfile .
+docker build -t terrabrasilis/dashboard-daily:alpha --build-arg INDEX_FILE=deter-cerrado-daily -f environment/Dockerfile .
+docker build -t terrabrasilis/dashboard-aggregated:alpha --build-arg INDEX_FILE=deter-cerrado-aggregated -f environment/Dockerfile .
+
+
+# If you want run containers, uncomment this lines
+#docker run -d --rm -p 80:80 --name terrabrasilis_dashboard_increments terrabrasilis/dashboard-increments:alpha
+#docker run -d --rm -p 83:80 --name terrabrasilis_dashboard_aggregated terrabrasilis/dashboard-aggregated:alpha
+#docker run -d --rm -p 84:80 --name terrabrasilis_dashboard_daily terrabrasilis/dashboard-daily:alpha
