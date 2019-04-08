bc1a9091b858ccd7405fa914d89f46ed9edf0491
André Carvalho <afacarvalho@yahoo.com.br>
2019-04-08 16:06:45 -0300

    Fix links and menu items.

--- docker-build.sh
+++ docker-build.sh
@@ -5,14 +5,14 @@
 #docker container stop terrabrasilis_cerrado_alert_daily
 
 # build all images
-docker build -t terrabrasilis/cerrado-alert-daily:v2.1-beta --build-arg INDEX_FILE=deter-cerrado-daily -f environment/Dockerfile .
-docker build -t terrabrasilis/cerrado-alert-aggregated:v2.1-beta --build-arg INDEX_FILE=deter-cerrado-aggregated -f environment/Dockerfile .
+docker build -t terrabrasilis/cerrado-alert-daily:v2.2-beta --build-arg INDEX_FILE=deter-cerrado-daily -f environment/Dockerfile .
+docker build -t terrabrasilis/cerrado-alert-aggregated:v2.2-beta --build-arg INDEX_FILE=deter-cerrado-aggregated -f environment/Dockerfile .
 
 # send to dockerhub
 # docker login
-docker push terrabrasilis/cerrado-alert-daily:v2.1-beta
-docker push terrabrasilis/cerrado-alert-aggregated:v2.1-beta
+docker push terrabrasilis/cerrado-alert-daily:v2.2-beta
+docker push terrabrasilis/cerrado-alert-aggregated:v2.2-beta
 
 # If you want run containers, uncomment this lines
-#docker run -d --rm -p 83:80 --name terrabrasilis_cerrado_alert_aggregated terrabrasilis/cerrado-alert-aggregated:v2.1-beta
-#docker run -d --rm -p 84:80 --name terrabrasilis_cerrado_alert_daily terrabrasilis/cerrado-alert-daily:v2.1-beta
+#docker run -d --rm -p 83:80 --name terrabrasilis_cerrado_alert_aggregated terrabrasilis/cerrado-alert-aggregated:v2.2-beta
+#docker run -d --rm -p 84:80 --name terrabrasilis_cerrado_alert_daily terrabrasilis/cerrado-alert-daily:v2.2-beta
