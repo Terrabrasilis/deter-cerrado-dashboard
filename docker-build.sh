7c06d286f6beb03a2a03e3f43b3f4911d0c0f496
Claudio Henrique Bogossian <claudio.bogossian@gmail.com>
2020-03-16 14:55:59 -0300

    Adding version to dashboard

--- docker-build.sh
+++ docker-build.sh
@@ -4,7 +4,7 @@
 #docker container stop terrabrasilis_cerrado_alert_aggregated
 #docker container stop terrabrasilis_cerrado_alert_daily
 
-VERSION=$(cat PROJECT_VERSION | grep -oP '(?<="version": ")[^"]*')
+VERSION=$(cat webapp/PROJECT_VERSION | grep -oP '(?<="version": ")[^"]*')
 
 # build all images
 docker build -t terrabrasilis/cerrado-alert-daily:v$VERSION --build-arg INDEX_FILE=deter-cerrado-daily -f environment/Dockerfile .
