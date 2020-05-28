3ca2e1a752b87ac3f08d89dbd8a7c0477d84372d
André Carvalho <afacarvalho@yahoo.com.br>
2020-05-28 16:08:11 -0300

    Upgrade the patch version

--- docker-build.sh
+++ docker-build.sh
@@ -5,14 +5,14 @@
 #docker container stop terrabrasilis_cerrado_alert_daily
 
 # build all images
-docker build -t terrabrasilis/cerrado-alert-daily:v2.5 --build-arg INDEX_FILE=deter-cerrado-daily -f environment/Dockerfile .
-docker build -t terrabrasilis/cerrado-alert-aggregated:v2.5 --build-arg INDEX_FILE=deter-cerrado-aggregated -f environment/Dockerfile .
+docker build -t terrabrasilis/cerrado-alert-daily:v2.5.1 --build-arg INDEX_FILE=deter-cerrado-daily -f environment/Dockerfile .
+docker build -t terrabrasilis/cerrado-alert-aggregated:v2.5.1 --build-arg INDEX_FILE=deter-cerrado-aggregated -f environment/Dockerfile .
 
 # send to dockerhub
 # docker login
-docker push terrabrasilis/cerrado-alert-daily:v2.5
-docker push terrabrasilis/cerrado-alert-aggregated:v2.5
+docker push terrabrasilis/cerrado-alert-daily:v2.5.1
+docker push terrabrasilis/cerrado-alert-aggregated:v2.5.1
 
 # If you want run containers, uncomment this lines
-#docker run -d --rm -p 83:80 --name terrabrasilis_cerrado_alert_aggregated terrabrasilis/cerrado-alert-aggregated:v2.5
-#docker run -d --rm -p 84:80 --name terrabrasilis_cerrado_alert_daily terrabrasilis/cerrado-alert-daily:v2.5
+#docker run -d --rm -p 83:80 --name terrabrasilis_cerrado_alert_aggregated terrabrasilis/cerrado-alert-aggregated:v2.5.1
+#docker run -d --rm -p 84:80 --name terrabrasilis_cerrado_alert_daily terrabrasilis/cerrado-alert-daily:v2.5.1
