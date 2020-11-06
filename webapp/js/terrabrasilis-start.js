2fa95f89519fb04f5766417efc1c9f7e58105620
André Carvalho <afacarvalho@yahoo.com.br>
2020-11-06 14:33:15 -0300

    Adding new item into top bar menu

--- webapp/js/terrabrasilis-start.js
+++ webapp/js/terrabrasilis-start.js
@@ -27,22 +27,22 @@ $(document).ready(function () {
 /* Altera icone do accordion da tabela */
 
 $(document).ready(function () {
-            $('.collapse')
-                .on('shown.bs.collapse', function() {
-                    $(this)
-                        .parent()
-                        .find(".fa-plus-circle")
-                        .removeClass("fa-plus-circle")
-                        .addClass("fa-minus-circle");
-                })
-                .on('hidden.bs.collapse', function() {
-                    $(this)
-                        .parent()
-                        .find(".fa-minus-circle")
-                        .removeClass("fa-minus-circle")
-                        .addClass("fa-plus-circle");
-                });
+    $('.collapse')
+        .on('shown.bs.collapse', function() {
+            $(this)
+            .parent()
+            .find(".fa-plus-circle")
+            .removeClass("fa-plus-circle")
+            .addClass("fa-minus-circle");
+        })
+        .on('hidden.bs.collapse', function() {
+            $(this)
+            .parent()
+            .find(".fa-minus-circle")
+            .removeClass("fa-minus-circle")
+            .addClass("fa-plus-circle");
         });
+});
 
 $(document).ready(function () {
 
