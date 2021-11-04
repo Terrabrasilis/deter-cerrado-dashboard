7af72469fadf6bfc6db103fd82bcbdf67d350b1d
André Carvalho <afacarvalho@yahoo.com.br>
2021-11-04 16:25:29 -0300

    Fix build script

--- docker-build.sh
+++ docker-build.sh
@@ -7,13 +7,13 @@
 VERSION=$(git describe --tags --abbrev=0)
 export VERSION
 # build all images
-docker build -t terrabrasilis/cerrado-alert-daily:v$VERSION --build-arg VERSION=$VERSION --build-arg INDEX_FILE=deter-cerrado-daily -f environment/Dockerfile .
-docker build -t terrabrasilis/cerrado-alert-aggregated:v$VERSION --build-arg VERSION=$VERSION --build-arg INDEX_FILE=deter-cerrado-aggregated -f environment/Dockerfile .
+docker build -t terrabrasilis/cerrado-alert-daily:$VERSION --build-arg VERSION=$VERSION --build-arg INDEX_FILE=deter-cerrado-daily -f environment/Dockerfile .
+docker build -t terrabrasilis/cerrado-alert-aggregated:$VERSION --build-arg VERSION=$VERSION --build-arg INDEX_FILE=deter-cerrado-aggregated -f environment/Dockerfile .
 
 # send to dockerhub
 # docker login
-docker push terrabrasilis/cerrado-alert-daily:v$VERSION
-docker push terrabrasilis/cerrado-alert-aggregated:v$VERSION
+docker push terrabrasilis/cerrado-alert-daily:$VERSION
+docker push terrabrasilis/cerrado-alert-aggregated:$VERSION
 
 # If you want run containers, uncomment this lines
 #docker run -d --rm -p 83:80 --name terrabrasilis_cerrado_alert_aggregated terrabrasilis/cerrado-alert-aggregated:v2.5
